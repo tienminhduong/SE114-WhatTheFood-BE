7905f0b34b0a61e9db220ae212af8e928f3a336c
TienMinhDuong <tmd31052005@gmail.com>
2025-06-18 14:20:56 +0700

    Add users/info

--- FoodAPI/Models/UserDto.cs
+++ FoodAPI/Models/UserDto.cs
@@ -2,7 +2,7 @@
 
 public class UserDto
 {
-    public int Id { get; set; }
     public string PhoneNumber { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
+    public string Role {  get; set; } = string.Empty;
 }
